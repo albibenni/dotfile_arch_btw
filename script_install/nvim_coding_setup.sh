4a72b1d202d0911a9e544b26ee67d6527774dc65
Alberto Benatti <albi.benni8@gmail.com>
2025-06-30 22:53:21 +0200

    nvim wip script

---
+++ script_install/nvim_coding_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo pacman -S nodejs npm ripgrep
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+rustup update
+
+GO_VERSION=1.24.4
+GO_TARBALL=go$GO_VERSION.linux-amd64.tar.gz
+GO_URL=https://golang.org/dl/$GO_TARBALL
+
+# Download Go tarball
+curl -LO $GO_URL
+
+# Remove any existing Go installation in /usr/local
+sudo rm -rf /usr/local/go
+
+# Extract the tarball to /usr/local
+sudo tar -C /usr/local -xzf $GO_TARBALL
+
+# (Optional) Clean up the tarball
+rm $GO_TARBALL
+
+# Add Go to PATH in current session (and suggest adding it to profile)
+export PATH=$PATH:/usr/local/go/bin
+echo "Go installed to /usr/local/go"
+echo "Add 'export PATH=\$PATH:/usr/local/go/bin' to your ~/.bashrc or ~/.zshrc"
