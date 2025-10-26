322f348bebbc99ade023ffc332170ee1f4afe4a2
Alberto Benatti <albi.benni8@gmail.com>
2025-10-26 21:02:30 +0100

    plugins script install

--- script_install/tmux.sh
+++ script_install/tmux.sh
@@ -5,3 +5,13 @@ sudo pacman -S tmux
 
 #tmp plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+
+mkdir -p ~/.config/tmux/plugins/catppuccin
+mkdir -p ~/.config/tmux/plugins/tmux-plugins/tmux-cpu
+
+# check version on:
+# https://github.com/catppuccin/tmux
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
+git clone https://github.com/tmux-plugins/tmux-cpu ~/.config/tmux/plugins/tmux-plugins/tmux-cpu/
+
