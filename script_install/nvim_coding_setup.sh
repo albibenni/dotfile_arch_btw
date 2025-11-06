132ac362d0786d40af9e0ea255093ae9bfa7982b
Alberto Benatti <albi.benni8@gmail.com>
2025-11-06 18:16:05 +0100

    fix package and comment rust things

--- script_install/nvim_coding_setup.sh
+++ script_install/nvim_coding_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo pacman -S nodejs npm ripgrep gcc build-essential lazygit go
+sudo pacman -S nodejs npm ripgrep gcc base-devel lazygit go
 
 sudo npm install -g stylelint stylelint-config-standard
 
 # rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-rustup update
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#
+# rustup update
 
 echo "Go installed to /usr/local/go"
 echo "Add 'export PATH=\$PATH:/usr/local/go/bin' to your ~/.bashrc or ~/.zshrc"
