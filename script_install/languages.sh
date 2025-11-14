3a4dc1f8c1b704b5d699aa95fa38f0466a6efe31
Alberto Benatti <albi.benni8@gmail.com>
2025-11-14 16:05:27 +0100

    add maven and java stuff

--- script_install/languages.sh
+++ script_install/languages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Just Node.js for web development LSPs
-sudo pacman -S nodejs npm go rustup pnpm lua51
+sudo pacman -S nodejs npm go rustup pnpm lua51 jre-openjdk maven
 
 rustup default stable
