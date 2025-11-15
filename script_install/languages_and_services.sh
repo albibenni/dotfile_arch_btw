8b08acc979fdf33d3f837b4713d305c4fcade642
Alberto Benatti <albi.benni8@gmail.com>
2025-11-15 10:08:36 +0100

    rn and add kafka installation

--- script_install/languages_and_services.sh
+++ script_install/languages_and_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Just Node.js for web development LSPs
-sudo pacman -S nodejs npm go rustup pnpm lua51 jre-openjdk maven
+sudo pacman -S nodejs npm go rustup pnpm lua51 jre-openjdk maven kafka
 
 rustup default stable
