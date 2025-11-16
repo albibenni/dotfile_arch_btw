152f803a6b61ebcab83d471b391a060d8eb43751
Alberto Benatti <albi.benni8@gmail.com>
2025-11-16 11:17:04 +0100

    add gradle to script

--- script_install/languages_and_services.sh
+++ script_install/languages_and_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Just Node.js for web development LSPs
-sudo pacman -S nodejs npm go rustup pnpm lua51 jre-openjdk maven kafka
+sudo pacman -S nodejs npm go rustup pnpm lua51 jre-openjdk maven gradle kafka
 
 rustup default stable
