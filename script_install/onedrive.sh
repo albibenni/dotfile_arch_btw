1f474530473d697fae467c13f2cc2e5b9f14b5ce
Alberto Benatti <albi.benni8@gmail.com>
2025-11-21 17:41:10 +0100

    add onedrive

---
+++ script_install/onedrive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# yay -S onedrive-abraunegg
+
+# run
+#onedrive # to sync it
+
+# check for error:
+# onedrive --synchronize --verbose --dry-run
+
+# run:
+# onedrive --sync  #to sync files
+
+
+## NOTE: other option onedriver
+yay -S onedriver
