0a89b22c3fc0a8c8434f5ed5c8edea878cd3682c
Alberto Benatti <albi.benni8@gmail.com>
2025-12-05 18:19:28 +0100

    working

--- exec_scripts/.local/share/bin/launch-floating-terminal-with-presentation.sh
+++ exec_scripts/.local/share/bin/launch-floating-terminal-with-presentation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cmd="$*"
-exec setsid uwsm-app -- xdg-terminal-exec --app-id=org.albibenni.terminal -e bash -c "show-logo.sh; $cmd; show-done.sh"
+exec setsid uwsm-app -- xdg-terminal-exec --app-id=org.albibenni.terminal --title=Albibenni -e bash -c "show-logo.sh; $cmd; show-done.sh"
