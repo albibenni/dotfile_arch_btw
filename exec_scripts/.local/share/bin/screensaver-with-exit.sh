681e15c8bc9f8814f812bc7f28bdfd7f3aa235cc
Alberto Benatti <albi.benni8@gmail.com>
2025-12-09 11:24:41 +0100

    loop the screensaver

--- exec_scripts/.local/share/bin/screensaver-with-exit.sh
+++ exec_scripts/.local/share/bin/screensaver-with-exit.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Run tte in background
-tte --input-file "$HOME/.local/share/arch/logo.txt" \
-    --canvas-width 0 \
-    --canvas-height 0 \
-    --anchor-canvas c \
-    --anchor-text c \
-    --random-effect &
+# Run tte in a loop in background
+(
+  while true; do
+    clear
+    tte --input-file "$HOME/.local/share/arch/logo.txt" \
+        --canvas-width 0 \
+        --canvas-height 0 \
+        --anchor-canvas c \
+        --anchor-text c \
+        --random-effect
+    sleep 1
+  done
+) &
 
-tte_pid=$!
+loop_pid=$!
 
 # Wait for any keypress
 read -n 1 -s
 
-# Kill tte when key is pressed
-kill $tte_pid 2>/dev/null
+# Kill the loop when key is pressed
+kill $loop_pid 2>/dev/null
+pkill -P $loop_pid 2>/dev/null
