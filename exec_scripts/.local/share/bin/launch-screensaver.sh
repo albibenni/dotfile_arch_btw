1d13f7e9e3eb0b225fa169bee51da126c9e71e8b
Alberto Benatti <albi.benni8@gmail.com>
2026-01-14 15:55:48 +0100

    refactor screensaver launch and id

--- exec_scripts/.local/share/bin/launch-screensaver.sh
+++ exec_scripts/.local/share/bin/launch-screensaver.sh
@@ -2,15 +2,15 @@
 
 # Exit early if we don't have the tte show
 if ! command -v tte &>/dev/null; then
-  exit 1
+    exit 1
 fi
 
 # Use Arch Linux logo for screensaver
 screensaver_file="$HOME/.local/share/arch/logo.txt"
 
 if [[ ! -f "$screensaver_file" ]]; then
-  echo "Arch logo not found. Run the logo.sh script first."
-  exit 1
+    echo "Arch logo not found. Run the logo.sh script first."
+    exit 1
 fi
 
 # Exit early if screensave is already running
@@ -18,7 +18,7 @@ pgrep -f org.albibenni.screensaver && exit 0
 
 # Allow screensaver to be turned off but also force started
 if [[ -f ~/.local/state/benni/toggles/screensaver-off ]] && [[ $1 != "force" ]]; then
-  exit 1
+    exit 1
 fi
 
 focused=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true).name')
@@ -28,49 +28,52 @@ terminal=$(xdg-terminal-exec --print-id)
 monitor_count=$(hyprctl monitors -j | jq -r '.[] | .name' | wc -l)
 
 for m in $(hyprctl monitors -j | jq -r '.[] | .name'); do
-  hyprctl dispatch focusmonitor $m
+    hyprctl dispatch focusmonitor $m
 
-  case $terminal in
-  *Alacritty*)
-    hyprctl dispatch exec -- \
-      alacritty --class=org.albibenni.screensaver \
-      --config-file ~/.config/alacritty/screensaver.toml \
-      -e screensaver-with-exit.sh
-    ;;
-  *ghostty*)
-    hyprctl dispatch exec -- \
-      ghostty --class=org.albibenni.screensaver \
-      --font-size=32 \
-      -e screensaver-with-exit.sh
-    ;;
-  *kitty*)
-    hyprctl dispatch exec -- \
-      kitty --class=org.albibenni.screensaver \
-      --override font_size=32 \
-      --override window_padding_width=0 \
-      -e screensaver-with-exit.sh
-    ;;
-  *)
-    notify-send "✋  Screensaver only runs in Alacritty, Ghostty, or Kitty"
-    ;;
-  esac
+    case $terminal in
+    *Alacritty*)
+        hyprctl dispatch exec -- \
+            alacritty --class=org.albibenni.screensaver \
+            --config-file ~/.config/alacritty/screensaver.toml \
+            -e screensaver-with-exit.sh
+        # -o window.startup_mode=Fullscreen \
+        ;;
+    *ghostty*)
+        hyprctl dispatch exec -- \
+            ghostty --class=org.albibenni.screensaver \
+            --font-size=32 \
+            -e screensaver-with-exit.sh
+        # --fullscreen=true \
+        ;;
+    *kitty*)
+        hyprctl dispatch exec -- \
+            kitty --class=org.albibenni.screensaver \
+            --override font_size=32 \
+            --override window_padding_width=0 \
+            -e screensaver-with-exit.sh
+        # --start-as=fullscreen \
+        ;;
+    *)
+        notify-send "✋  Screensaver only runs in Alacritty, Ghostty, or Kitty"
+        ;;
+    esac
 done
 
 hyprctl dispatch focusmonitor $focused
 
 # Monitor screensaver instances - kill all if any one closes
 (
-  sleep 2  # Give time for all instances to start
-  while true; do
-    current_count=$(pgrep -fc org.albibenni.screensaver)
-    if [[ $current_count -gt 0 && $current_count -lt $monitor_count ]]; then
-      # One or more closed, kill the rest
-      pkill -f org.albibenni.screensaver
-      break
-    elif [[ $current_count -eq 0 ]]; then
-      # All closed
-      break
-    fi
-    sleep 0.5
-  done
+    sleep 2 # Give time for all instances to start
+    while true; do
+        current_count=$(pgrep -fc org.albibenni.screensaver)
+        if [[ $current_count -gt 0 && $current_count -lt $monitor_count ]]; then
+            # One or more closed, kill the rest
+            pkill -f org.albibenni.screensaver
+            break
+        elif [[ $current_count -eq 0 ]]; then
+            # All closed
+            break
+        fi
+        sleep 0.5
+    done
 ) &
--- exec_scripts/.local/share/bin/screensaver-with-exit.sh
+++ exec_scripts/.local/share/bin/screensaver-with-exit.sh
@@ -8,34 +8,47 @@
 
 # Array of available logos
 logos=(
-  "$HOME/.local/share/arch/logo.txt"
-  "$HOME/.local/share/arch/logo1.txt"
-  "$HOME/.local/share/arch/logo2.txt"
-  "$HOME/.local/share/arch/logo3.txt"
+    "$HOME/.local/share/arch/logo.txt"
+    "$HOME/.local/share/arch/logo1.txt"
+    "$HOME/.local/share/arch/logo2.txt"
+    "$HOME/.local/share/arch/logo3.txt"
 )
+screensaver_in_focus() {
+    hyprctl activewindow -j | jq -e '.class == "org.albibenni.screensaver"' >/dev/null 2>&1
+}
+
+exit_screensaver() {
+    hyprctl keyword cursor:invisible false &>/dev/null || true
+    pkill -x tte 2>/dev/null
+    pkill -f org.albibenni.screensaver 2>/dev/null
+    exit 0
+}
+
+# Exit the screensaver on signals and input from keyboard and mouse
+trap exit_screensaver SIGINT SIGTERM SIGHUP SIGQUIT
+hyprctl keyword cursor:invisible true &>/dev/null
+tty=$(tty 2>/dev/null)
 
 # Run tte in a loop in background
-(
-  while true; do
+while true; do
     # Pick a random logo
     random_logo="${logos[$RANDOM % ${#logos[@]}]}"
 
-    clear
-    tte --input-file "$random_logo" \
-        --canvas-width 0 \
-        --canvas-height 0 \
-        --anchor-canvas c \
-        --anchor-text c \
-        --random-effect
-    sleep 1
-  done
-) &
+    tte -i "$random_logo" \
+        --frame-rate 120 --canvas-width 0 --canvas-height 0 --reuse-canvas --anchor-canvas c --anchor-text c --random-effect --exclude-effects dev_worm \
+        --no-eol --no-restore-cursor &
+    while pgrep -t "${tty#/dev/}" -x tte >/dev/null; do
+        if read -n1 -t 1 || ! screensaver_in_focus; then
+            exit_screensaver
+        fi
+    done
+done
 
-loop_pid=$!
-
-# Wait for any keypress
-read -n 1 -s
-
-# Kill the loop when key is pressed
-kill $loop_pid 2>/dev/null
-pkill -P $loop_pid 2>/dev/null
+# loop_pid=$!
+#
+# # Wait for any keypress
+# read -n 1 -s
+#
+# # Kill the loop when key is pressed
+# kill $loop_pid 2>/dev/null
+# pkill -P $loop_pid 2>/dev/null
