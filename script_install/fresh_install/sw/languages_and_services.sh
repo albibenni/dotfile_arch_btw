02250d40c6c71ee141e1145c0a96011e5a9c7474
Alberto Benatti <albi.benni8@gmail.com>
2026-05-20 22:02:38 +0200

    needed

--- script_install/fresh_install/sw/languages_and_services.sh
+++ script_install/fresh_install/sw/languages_and_services.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install node via fnm
-sudo pacman -S go rustup lua51 jdk-openjdk maven gradle kafka
+sudo pacman -S --needed go rustup lua51 jdk-openjdk maven gradle kafka
 
-yay -S bun
+yay -S --needed bun
 
 rustup default stable
