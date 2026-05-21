5fa6f883fd75ccc58c85dba5d60de417b50979aa
Alberto Benatti <albi.benni8@gmail.com>
2026-05-21 16:14:37 +0200

    torrent install with ui

--- script_install/fresh_install/apps/torrent.sh
+++ script_install/fresh_install/apps/torrent.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo pacman -S --needed qbittorrent-nox
+# Check if qbittorrent-nox is installed and remove it to prefer the GUI version
+if pacman -Qs qbittorrent-nox > /dev/null; then
+    echo "Transitioning from qbittorrent-nox to qbittorrent GUI..."
+    sudo pacman -Rs --noconfirm qbittorrent-nox
+fi
+
+sudo pacman -S --needed --noconfirm qbittorrent
